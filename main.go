867c004dd61b08403542253af1f89701254ac9cb
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-07-26 21:26:38 +0700

    hadi'a: main.go

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
     "todo-api/routes"
     "github.com/gofiber/fiber/v2"
+    "todo-api/database"
 )
 
 func main() {
+    database.Connect()
     app := fiber.New()
     routes.Setup(app)
     app.Listen(":3000")
